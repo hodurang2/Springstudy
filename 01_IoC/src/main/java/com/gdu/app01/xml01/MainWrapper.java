f51f51f224df31ffbec4830d4b219be77474de1f
hodurang2 <cchc0427@gmail.com>
2023-09-25 11:53:44 +0900

    20230925

---
+++ 01_IoC/src/main/java/com/gdu/app01/xml01/MainWrapper.java
@@ -0,0 +1,30 @@
+package com.gdu.app01.xml01;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    // app-context.xml 파일 읽기 (여기에서 <bean> 태그로 정의해 둔 객체가 생성된다.)
+    AbstractApplicationContext ctx = new GenericXmlApplicationContext("xml01/app-context.xml");
+    
+    // <bean> 태그로 정의된 객체 가져오기
+    Calculator calculator = (Calculator)ctx.getBean("calc");  // ctx.getBean("cacl", Calculator.class);
+    
+    // 객체 사용해 보기
+    calculator.add(1, 2);
+    calculator.sub(3, 4);
+    calculator.mul(5, 6);
+    calculator.div(7, 8);
+    
+    // app-context.xml 파일 닫기
+    ctx.close();
+    
+    
+    
+    
+  }
+
+}
