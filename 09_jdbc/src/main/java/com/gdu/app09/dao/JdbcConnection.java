5766e7ca66b059d608defac145b0dfba40d459cf
hodurang2 <cchc0427@gmail.com>
2023-10-10 17:40:26 +0900

    20231010

---
+++ 09_jdbc/src/main/java/com/gdu/app09/dao/JdbcConnection.java
@@ -0,0 +1,48 @@
+package com.gdu.app09.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.springframework.stereotype.Component;
+
+// JdbcConnection을 컨테이너에 만들어두겠다 ! 방법은  셋 중 하나임 - <bean> or @Bean or @Component -> @Component 사용하겠다 !
+@Component    // JdbcConnection 객체(Bean)를 Spring Container에 저장해 둔다.
+public class JdbcConnection {
+
+  /*
+   * 일반 버전
+   *  Class.forName("oracle.jdbc.OracleDriver");
+   *  url = "jdbc:oracle:thin:@localhost:1521:xe"
+   * 
+   * 쿼리 출력 버전(log4jdbc 디펜던시)
+   *  Class.forName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy."); 
+   *  url = "jdbc:log4jdbc:oracle:thin:@localhost:1521:xe"  
+   */
+  
+  
+  public Connection getConnection() {
+    Connection con = null;
+    try {
+      Class.forName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+      con = DriverManager.getConnection("jdbc:log4jdbc:oracle:thin:@localhost:1521:xe", "GD", "1111");
+      
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    return con;
+  }
+  
+  public void close(Connection con, PreparedStatement ps, ResultSet rs) {
+    
+    try {
+      if(rs != null) rs.close();
+      if(ps != null) ps.close();
+      if(con != null) con.close();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+}
--- 09_jdbc_junit/src/main/java/com/gdu/app09/controller/MvcController.java
+++
@@ -1,5 +0,0 @@
-package com.gdu.app09.controller;
-
-public class MvcController {
-
-}
